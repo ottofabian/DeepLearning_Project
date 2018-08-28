47cdd8993b22f478f6ec91278b3d5df7e3e52f1f
franzklein <franz.klein@outlook.com>
2018-08-28 17:31:30 +0200

    predict_Pixelnet.py: small update

--- predict_Pixelnet.py
+++ predict_Pixelnet.py
@@ -7,20 +7,24 @@ from CityscapesHandler import CityscapesHandler
 from PixelNet import PixelNet
 
 
-def draw_results(image, idx, res):
+def label_to_color(num_classes, value):
+    num_classes = float(num_classes)
+    ratio = 2 * value / num_classes
+    b = int(max(0, 255*(1 - ratio)))
+    r = int(max(0, 255*(ratio - 1)))
+    g = 255 - b - r
+    return r, g, b
+
+
+def draw_results(image, res, num_classes):
     h = image.shape[0]
     w = image.shape[1]
     for k in range(0, h):
         for i in range(0, w):
-            image[k, i, 0] = 0
-            image[k, i, 1] = 0
-            image[k, i, 2] = 0
-            if(res[k * w + i] < 10):
-                image[k, i, 0] = res[k * w + i] / 10.0 * 255
-            elif(res[k * w + i] < 20):
-                image[k, i, 1] = res[k * w + i] / 10.0 * 255
-            else:
-                image[k, i, 2] = res[k * w + i] / 10.0 * 255
+            r, g, b = label_to_color(num_classes, res[k * w + i])
+            image[k, i, 0] = r
+            image[k, i, 1] = g
+            image[k, i, 2] = b
     
     return image
     
@@ -39,15 +43,11 @@ input_image_shape = train_x[0].shape
 
 with tf.Graph().as_default():
     images = tf.placeholder(tf.float32, shape=[1, input_image_shape[0], input_image_shape[1], 3], name='images')
-
-    
-    #preprocess
-    r, g, b = tf.split(axis=3, num_or_size_splits=3, value=images)
-    VGG_MEAN = [103.939, 116.779, 123.68]
-    bgr = tf.concat(values=[b - VGG_MEAN[0], g - VGG_MEAN[1], r - VGG_MEAN[2]], axis=3)   
     
     pn = PixelNet()
-    logits = pn.build(images=bgr, num_classes=n_classes)
+    train_bgr_norm = pn.preprocess_images(images)
+    
+    logits = pn.build(images=train_bgr_norm, num_classes=n_classes)
     predictions = tf.argmax(logits, 1)
 
     #load model
@@ -57,15 +57,13 @@ with tf.Graph().as_default():
     ckpt = tf.train.get_checkpoint_state(model_path)
     saver.restore(sess, ckpt.model_checkpoint_path)  
 
-    feed_dict = {images: train_x}
-
     print("start prediction")
-    res = sess.run([predictions], feed_dict=feed_dict)
+    res = sess.run([predictions], feed_dict={images: train_x})
     res = res[0]
 
     print("prediction done - draw results")
 
     result_image = np.full((input_image_shape[0], input_image_shape[1], 3), 255)
-    result_image = draw_results(result_image, None, res)
+    result_image = draw_results(result_image, res, n_classes)
             
     csh.displayImage(np.concatenate((train_x[0], result_image), axis=0).astype(np.uint8))
