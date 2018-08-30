fca22ca3740aff7fa084298f9e3fc4eb5d70bbaa
Fabian Otto <ottofab@schaeffler.com>
2018-08-30 10:20:24 +0200

    Added iou test eval and some visualizations

--- PixelNet.py
+++ PixelNet.py
@@ -4,64 +4,63 @@ import tensorflow.contrib.slim as slim
 
 from vgg16 import vgg_16
 
-class PixelNet:
 
+class PixelNet:
     def generate_sample_idxs(self, input_image_shape, n_images, pixel_sample_size):
         idx = np.empty((0, 3))
         for k in range(n_images):
-            pxl_idx = np.random.choice(input_image_shape[0] * input_image_shape[1], size=pixel_sample_size, replace=False).reshape(pixel_sample_size, 1)
-            pxl_idx = np.concatenate((pxl_idx / input_image_shape[1], pxl_idx % input_image_shape[1]), axis=1).astype(np.int)
+            pxl_idx = np.random.choice(input_image_shape[0] * input_image_shape[1], size=pixel_sample_size,
+                                       replace=False).reshape(pixel_sample_size, 1)
+            pxl_idx = np.concatenate((pxl_idx / input_image_shape[1], pxl_idx % input_image_shape[1]), axis=1).astype(
+                np.int)
             img_idx = np.full(pixel_sample_size, k)
             comb = np.insert(pxl_idx, 0, img_idx, axis=1)
             idx = np.append(idx, comb, axis=0)
         return idx.astype(np.int)
-        
-        
+
     def preprocess_images(self, images):
         r, g, b = tf.split(axis=3, num_or_size_splits=3, value=images)
         VGG_MEAN = [103.939, 116.779, 123.68]
         return tf.concat(values=[b - VGG_MEAN[0], g - VGG_MEAN[1], r - VGG_MEAN[2]], axis=3)
-              
-  
-    def interpolate_bilinear(self, I, C):    
+
+    def interpolate_bilinear(self, I, C):
         top_left = tf.cast(tf.floor(C), tf.int32)
         top_right = tf.cast(tf.concat([C[:, 0:1], tf.floor(C[:, 1:2]), tf.ceil(C[:, 2:3])], 1), tf.int32)
         bottom_left = tf.cast(tf.concat([C[:, 0:1], tf.ceil(C[:, 1:2]), tf.floor(C[:, 2:3])], 1), tf.int32)
         bottom_right = tf.cast(tf.ceil(C), tf.int32)
-        
-        values_at_top_left = tf.gather_nd(I, top_left)    
+
+        values_at_top_left = tf.gather_nd(I, top_left)
         values_at_top_right = tf.gather_nd(I, top_right)
         values_at_bottom_left = tf.gather_nd(I, bottom_left)
         values_at_bottom_right = tf.gather_nd(I, bottom_right)
-        
+
         # Varies between 0.0 and 1.0.
-        horizontal_offset = C[:, 1] - tf.cast(top_left[:, 1], tf.float32) 
-        horizontal_offset= tf.reshape(horizontal_offset, [-1, 1])        
+        horizontal_offset = C[:, 1] - tf.cast(top_left[:, 1], tf.float32)
+        horizontal_offset = tf.reshape(horizontal_offset, [-1, 1])
         horizontal_offset = tf.tile(horizontal_offset, [1, I.shape[3]])
-        
+
         horizontal_interpolated_top = (
             ((1.0 - horizontal_offset) * values_at_top_left)
             + (horizontal_offset * values_at_top_right))
-            
+
         horizontal_interpolated_bottom = (
             ((1.0 - horizontal_offset) * values_at_bottom_left)
-            + (horizontal_offset * values_at_bottom_right))            
-            
-        vertical_offset = C[:, 2] - tf.cast(top_left[:, 2], tf.float32) 
-        vertical_offset= tf.reshape(vertical_offset, [-1, 1])
-        vertical_offset = tf.tile(vertical_offset, [1,I.shape[3]])
-        
+            + (horizontal_offset * values_at_bottom_right))
+
+        vertical_offset = C[:, 2] - tf.cast(top_left[:, 2], tf.float32)
+        vertical_offset = tf.reshape(vertical_offset, [-1, 1])
+        vertical_offset = tf.tile(vertical_offset, [1, I.shape[3]])
+
         interpolated_result = (
             ((1.0 - vertical_offset) * horizontal_interpolated_top)
             + (vertical_offset * horizontal_interpolated_bottom))
 
         return interpolated_result
 
-        
     def random_sampling(self, features, labels, index):
         with tf.name_scope('RandomSampling'):
-            shape = features[0].shape[1:-1]       
-            if index is None:                
+            shape = features[0].shape[1:-1]
+            if index is None:
                 upsampled = [features[0]]
                 for i in range(1, len(features)):
                     upsampled.append(tf.image.resize_bilinear(features[i], shape))
@@ -69,42 +68,43 @@ class PixelNet:
                 vector = tf.reshape(vector, (shape[0] * shape[1], vector.shape[3]))
                 label = None
             else:
-                #decide for one:
-                #------------------------------
-                #previous implementation (no sparsity):
+                # decide for one:
+                # ------------------------------
+                # previous implementation (no sparsity):
                 # upsampled = [features[0]]
                 # for i in range(1, len(features)):
-                    # upsampled.append(tf.image.resize_bilinear(features[i], shape))
-                #samples = [tf.gather_nd(feature, index) for feature in upsampled]
-                #------------------------------
-                #sparse sampling:
+                # upsampled.append(tf.image.resize_bilinear(features[i], shape))
+                # samples = [tf.gather_nd(feature, index) for feature in upsampled]
+                # ------------------------------
+                # sparse sampling:
                 samples = []
                 for f in features:
                     idx_image = tf.cast(index[:, 0], dtype=tf.float32)
-                    idx1 = tf.cast(index[:, 1] / features[0].shape[1], dtype=tf.float32) * (tf.cast(f.shape[1], dtype=tf.float32) - 1)
-                    idx2 = tf.cast(index[:, 2] / features[0].shape[2], dtype=tf.float32) * (tf.cast(f.shape[2], dtype=tf.float32) - 1)
-                    idx_scaled = tf.stack([idx_image, idx1, idx2], axis = 1)
-                    
-                    samples.append(self.interpolate_bilinear(f, idx_scaled))                    
-                #------------------------------ 
+                    idx1 = tf.cast(index[:, 1] / features[0].shape[1], dtype=tf.float32) * (
+                    tf.cast(f.shape[1], dtype=tf.float32) - 1)
+                    idx2 = tf.cast(index[:, 2] / features[0].shape[2], dtype=tf.float32) * (
+                    tf.cast(f.shape[2], dtype=tf.float32) - 1)
+                    idx_scaled = tf.stack([idx_image, idx1, idx2], axis=1)
+
+                    samples.append(self.interpolate_bilinear(f, idx_scaled))
+                    # ------------------------------
                 vector = tf.concat(samples, axis=-1)
                 label = tf.gather_nd(labels, index)
-            
-            return vector, label
 
+            return vector, label
 
     def build(self, images, num_classes, labels=None, index=None):
         with tf.name_scope('PixelNet'):
-            
+
             out, layers_out = vgg_16(images,
                                      num_classes=num_classes,
                                      is_training=True,
                                      dropout_keep_prob=0.5,
                                      spatial_squeeze=False,
                                      scope='vgg_16',
-                                     fc_conv_padding='SAME',
+                                     fc_conv_padding='VALID',
                                      global_pool=False)
-           
+
             features = []
             features.append(layers_out["vgg_16/conv1/conv1_2"])
             features.append(layers_out["vgg_16/conv2/conv2_2"])
@@ -115,11 +115,14 @@ class PixelNet:
 
             x, y = self.random_sampling(features, labels, index)
             with tf.name_scope('MLP'):
-                x = slim.fully_connected(x, 4096, activation_fn=tf.nn.relu, weights_initializer=tf.truncated_normal_initializer(0.0, 0.001), scope='fc1')
+                x = slim.fully_connected(x, 4096, activation_fn=tf.nn.relu,
+                                         weights_initializer=tf.truncated_normal_initializer(0.0, 0.001), scope='fc1')
                 x = slim.dropout(x, 0.5, scope='dropout1')
-                x = slim.fully_connected(x, 4096, activation_fn=tf.nn.relu, weights_initializer=tf.truncated_normal_initializer(0.0, 0.001), scope='fc2')
+                x = slim.fully_connected(x, 4096, activation_fn=tf.nn.relu,
+                                         weights_initializer=tf.truncated_normal_initializer(0.0, 0.001), scope='fc2')
                 x = slim.dropout(x, 0.5, scope='dropout2')
-                x = slim.fully_connected(x, num_classes, activation_fn=tf.nn.relu, weights_initializer=tf.truncated_normal_initializer(0.0, 0.001), scope='fc3')
+                x = slim.fully_connected(x, num_classes, activation_fn=tf.nn.relu,
+                                         weights_initializer=tf.truncated_normal_initializer(0.0, 0.001), scope='fc3')
             if labels is not None:
                 return x, y
             else:
--- train_PixelNet.py
+++ train_PixelNet.py
@@ -1,24 +1,25 @@
+import math
+
+from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
 
 from CityscapesHandler import CityscapesHandler
 from PixelNet import PixelNet
 
-   
-path_vgg16_vars = "./data/vgg_16.ckpt" #downloadable at https://github.com/tensorflow/models/tree/master/research/slim
+path_vgg16_vars = "./data/vgg_16.ckpt"  # downloadable at https://github.com/tensorflow/models/tree/master/research/slim
 model_save_path = "./model/pixelnet"
 
 n_train_images = 2975  # max: 2975
-n_val_images = 500    # max: 500
+n_val_images = 500  # max: 500
 size_batch = 5
-n_batches = int(n_train_images / size_batch)
-n_steps = 150
+n_batches = int(math.ceil(n_train_images / size_batch))  # if uneven the last few images are trained as well
+n_steps = 80
 n_classes = 30
-pixel_sample_size = 2000 # per image
-lr = 0.00003
-input_image_shape = (320, 160) #(width, height)
-valid_after_n_steps = 3
-
+pixel_sample_size = 10000 // size_batch  # per image
+lr = 1e-5
+input_image_shape = (224, 224)  # (width, height)
+valid_after_n_steps = 10
 
 csh = CityscapesHandler()
 train_x, train_y = csh.getTrainSet(n_train_images, shape=input_image_shape)
@@ -29,93 +30,153 @@ val_y = val_y[:, :, :, None]
 
 input_image_shape = train_x[0].shape
 
+graph = tf.Graph()
+with graph.as_default():
+    train_images = tf.placeholder(tf.float32, shape=[None, input_image_shape[0], input_image_shape[1], 3],
+                                  name='train_images')
+    train_labels = tf.placeholder(tf.int32, shape=[None, input_image_shape[0], input_image_shape[1], 1],
+                                  name='train_labels')
 
-with tf.Graph().as_default():
-    train_images = tf.placeholder(tf.float32, shape=[None, input_image_shape[0], input_image_shape[1], 3], name='train_images')
-    train_labels = tf.placeholder(tf.int32, shape=[None, input_image_shape[0], input_image_shape[1], 1], name='train_labels')
-    
-    val_images = tf.placeholder(tf.float32, shape=[None, input_image_shape[0], input_image_shape[1], 3], name='val_images')
-    val_labels = tf.placeholder(tf.int32, shape=[None, input_image_shape[0], input_image_shape[1], 1], name='val_labels')
-    
-    index = tf.placeholder(tf.int32, shape=[None, 3], name='index') 
+    val_images = tf.placeholder(tf.float32, shape=[None, input_image_shape[0], input_image_shape[1], 3],
+                                name='val_images')
+    val_labels = tf.placeholder(tf.int32, shape=[None, input_image_shape[0], input_image_shape[1], 1],
+                                name='val_labels')
+
+    index = tf.placeholder(tf.int32, shape=[None, 3], name='index')
     batch_size = tf.placeholder(tf.int64)
-    
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_images,train_labels)).shuffle(buffer_size=n_batches).batch(batch_size).repeat()
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(
+        buffer_size=n_batches).batch(batch_size).repeat()
     val_dataset = tf.data.Dataset.from_tensor_slices((val_images, val_labels)).batch(batch_size).repeat()
-    
-    
-    iter = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
-    
-    batch_images, batch_labels = iter.get_next()
-    train_init_op = iter.make_initializer(train_dataset)
-    val_init_op = iter.make_initializer(val_dataset)
-    
+
+    iterator = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
+
+    batch_images, batch_labels = iterator.get_next()
+    train_init_op = iterator.make_initializer(train_dataset)
+    val_init_op = iterator.make_initializer(val_dataset)
+
     pn = PixelNet()
-  
-    #preprocess
+
+    # preprocess
     train_bgr_norm = pn.preprocess_images(batch_images)
-  
+
     logits, y = pn.build(images=train_bgr_norm, num_classes=n_classes, labels=batch_labels, index=index)
     y_one_hot = tf.one_hot(y, n_classes)
-    y = tf.reshape(y, [-1])
-    
-    vgg_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='vgg_16')[:-2]   
+    # y = tf.reshape(y, [-1])
+    logits = tf.reshape(logits, (-1, n_classes))
+    y = tf.reshape(y_one_hot, (-1, n_classes))
+
+    vgg_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='vgg_16')[:-2]
     pixelnet_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-    
+
     predictions = tf.argmax(logits, 1)
 
-    
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_one_hot, logits=logits)
-    loss_mean = tf.reduce_mean(cross_entropy)
+    with tf.name_scope('Loss'):
+        # cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_one_hot, logits=logits)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=logits)
+        loss_mean = tf.reduce_mean(cross_entropy)
 
-    # iou_mean, conf_mat = tf.metrics.mean_iou (labels=y, predictions=predictions, num_classes=n_classes)
+    predicted_label = tf.argmax(logits, axis=-1)
+    sparse_correct_label = tf.argmax(y, axis=-1)
+
+    with tf.variable_scope("iou") as scope:
+        iou, iou_op = tf.metrics.mean_iou(sparse_correct_label, predicted_label, n_classes, name="iou_metric")
+    iou_metric_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="iou_metric")
+
+    # with tf.name_scope('iou'):
+    #     # tf.Print(y, [y],)
+    #     iou, conf_mat = tf.metrics.mean_iou(labels=y, predictions=predictions, num_classes=n_classes)
+    #     # tf.Print(iou, [iou], "IoU values")
+    #     # tf.Print(iou)
 
     optimizer = tf.train.AdamOptimizer(lr)
     train_op = optimizer.minimize(loss=cross_entropy, global_step=tf.train.get_global_step())
-    
-    #load VGG model
+
+with tf.Session(graph=graph) as sess:
+    # load VGG model
     saver = tf.train.Saver(vgg_vars)
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer()) 
-    sess.run(tf.local_variables_initializer())     
+
+    # iou_metric_vars_initializer = tf.variables_initializer(var_list=iou_metric_vars)
+    # sess.run(iou_metric_vars_initializer)
+
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
     saver.restore(sess, path_vgg16_vars)
 
-    print("start training")
+    # history container
+    loss_history = np.zeros((n_steps,))
+    iou_history = np.zeros((n_steps,))
+    loss_history_test = np.zeros((n_steps // valid_after_n_steps + 1,))
+    iou_history_test = np.zeros((n_steps // valid_after_n_steps + 1,))
+
+    print("Training started")
     try:
-    
-        sess.run(train_init_op, feed_dict={train_images: train_x, train_labels: train_y, batch_size: size_batch})        
+
+        sess.run(train_init_op, feed_dict={train_images: train_x, train_labels: train_y, batch_size: size_batch})
 
         for step in range(n_steps):
             tot_loss = 0
-            for _ in range(n_batches):           
-                idx = pn.generate_sample_idxs(input_image_shape, size_batch, pixel_sample_size)            
-                __, loss_value = sess.run([train_op, loss_mean], feed_dict={index: idx})
-                tot_loss += loss_value
-                print("--batch ", _, "/", n_batches)
-            print("step: {}, Loss: {:.4f}".format(step, tot_loss / n_batches))
+            tot_iou = 0
+            print("Epoch {}/{}".format(step + 1, n_steps))
+            for batch_step in range(n_batches):
+                idx = pn.generate_sample_idxs(input_image_shape, size_batch, pixel_sample_size)
+                _, loss_value, _ = sess.run([train_op, loss_mean, iou_op], feed_dict={index: idx})
+                loss_history[step] += loss_value
+                print("--batch {}/{}".format(batch_step + 1, n_batches))
 
-            if(step % valid_after_n_steps == 0):
-                sess.run(val_init_op, feed_dict={val_images: val_x, val_labels: val_y, batch_size: n_val_images})
-                
+            loss_history[step] /= n_batches
+            iou_history[step] /= n_batches
+
+            print("Training Loss: {:.4f} -- IoU: {:.4f}".format(loss_history[step], sess.run(iou)))
+
+            if step % valid_after_n_steps == 0:
+                sess.run([val_init_op], feed_dict={val_images: val_x, val_labels: val_y, batch_size: n_val_images})
+
                 idx = pn.generate_sample_idxs(input_image_shape, n_val_images, pixel_sample_size)
-                #sess.run([conf_mat], feed_dict={index: idx})
+                # sess.run([conf_mat], feed_dict={index: idx})
                 # loss_value, iou  = sess.run([loss_mean, iou_mean], feed_dict={index: idx})
-                loss_value = sess.run([loss_mean], feed_dict={index: idx})
-                loss_value = loss_value[0]
-                print("Validation Loss: {:.4f}".format(loss_value))
-                # print('Validation (IOU mean): {:4f}'.format(iou))
-                
-                sess.run(train_init_op, feed_dict={train_images: train_x, train_labels: train_y, batch_size: size_batch})
-                
+                loss_value, _ = sess.run([loss_mean, iou_op], feed_dict={index: idx})
+                loss_history_test[step // valid_after_n_steps] = loss_value
+                iou_history_test[step // valid_after_n_steps] = sess.run(iou)
+                print("Validation Loss: {:.4f} -- IoU: {:.4f}".format(loss_value, iou_history_test[
+                    step // valid_after_n_steps]))
+                # loss_value = loss_value[0]
+
+                sess.run(train_init_op,
+                         feed_dict={train_images: train_x, train_labels: train_y, batch_size: size_batch})
+
+                # save model checkpoint to disk
+                saver = tf.train.Saver(pixelnet_vars, save_relative_paths=True)
+                save_path = saver.save(sess,
+                                       "{}_step_{}_loss_{:.4f}".format(model_save_path, step, loss_history[step]))
+                print("Model checkpoint saved in path: {}".format(save_path))
+
     except KeyboardInterrupt:
-        #interrupts training process after pressing ctrl+c
+        # interrupts training process after pressing ctrl+c
         pass
-        
-    print("training done")
-    
-    #save new model to disk
-    print("start saving model")
-    saver = tf.train.Saver(pixelnet_vars, save_relative_paths = True)
-    save_path = saver.save(sess, model_save_path)
+
+    print("Training done")
+
+    # save new model to disk
+    print("Saving model")
+    saver = tf.train.Saver(pixelnet_vars, save_relative_paths=True)
+    save_path = saver.save(sess, "{}_final".format(model_save_path))
     print("Model saved in path: %s" % save_path)
 
+    plt.figure(0)
+    plt.plot(np.arange(1, n_steps + 1), loss_history, label="train")
+    plt.plot(np.arange(1, n_steps + 1), loss_history_test, label="test")
+    plt.title("Loss Progress")
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+
+    plt.figure(0)
+    plt.plot(np.arange(1, n_steps + 1), iou_history, label="train")
+    plt.plot(np.arange(1, n_steps // valid_after_n_steps + 2), iou_history_test, label="test")
+    plt.title("IoU Progress")
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("IoU")
+
+    plt.show()
